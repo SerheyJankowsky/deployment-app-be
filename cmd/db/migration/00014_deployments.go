889f8a350f8351626ac6b817f94d5d460c2a01b6
SerheyJ <s.typilow@gmail.com>
2025-06-12 17:47:54 +0300

    Replace deployment migration files with a new structure
    - Deleted the old migration file for creating deployments and replaced it with a new file that sets up the migration context for deployments.
    - The new migration file includes placeholder functions for applying and rolling back the migration, ensuring a cleaner and more organized approach to database migrations.

---
+++ cmd/db/migration/00014_deployments.go
@@ -0,0 +1,22 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigrationContext(upDeployments, downDeployments)
+}
+
+func upDeployments(ctx context.Context, tx *sql.Tx) error {
+	// This code is executed when the migration is applied.
+	return nil
+}
+
+func downDeployments(ctx context.Context, tx *sql.Tx) error {
+	// This code is executed when the migration is rolled back.
+	return nil
+}
