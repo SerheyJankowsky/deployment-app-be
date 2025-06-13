377f4b1015e8f0fa0708e78852e042aac8f6fcc6
SerheyJ <s.typilow@gmail.com>
2025-06-13 18:44:54 +0300

    Update User struct to include timestamps for created and updated records
    - Added CreatedAt and UpdatedAt fields to the User struct to automatically track record creation and modification times.
    - This change enhances data integrity and provides better auditing capabilities for user entities.

--- modules/users/user.entyty.go
+++ modules/users/user.entyty.go
@@ -1,12 +1,14 @@
 package users
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	// ID        uint   `gorm:"primaryKey" json:"id"`
+	ID        uint   `gorm:"primaryKey" json:"id"`
 	FirstName string `gorm:"not null" json:"first_name"`
 	LastName  string `gorm:"not null" json:"last_name"`
 	Username  string `gorm:"unique;not null" json:"username"`
@@ -23,7 +25,7 @@ type User struct {
 	PasswordHash string `gorm:"not null" json:"-"`
 	IV           string `gorm:"not null" json:"-"`
 
-	// CreatedAt    time.Time      `gorm:"autoCreateTime" json:"created_at"`
-	// UpdatedAt    time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 	// DeletedAt    gorm.DeletedAt `gorm:"index" json:"-"`
 }
