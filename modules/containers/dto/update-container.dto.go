f0c468b8c9d14fb44dc9007b5446b8ea65ab2d39
SerheyJ <s.typilow@gmail.com>
2025-06-14 20:36:16 +0300

    Update validation rules in UpdateContainerDto to allow nullable fields
    - Modified validation rules for Registry, Username, Password, SecretKey, and Params fields to permit empty values by changing the minimum length from 1 to 0.
    - This change enhances flexibility in the UpdateContainerDto, allowing for optional updates to these fields.

--- modules/containers/dto/update-container.dto.go
+++ modules/containers/dto/update-container.dto.go
@@ -4,13 +4,13 @@ import "github.com/go-playground/validator/v10"
 
 type UpdateContainerDto struct {
 	Name      *string `json:"name" validate:"omitempty,min=1,max=255"`
-	Registry  *string `json:"registry" validate:"omitempty,min=1,max=255"`
+	Registry  *string `json:"registry" validate:"omitempty,min=0,max=255"`
 	Image     *string `json:"image" validate:"omitempty,min=1,max=255"`
 	Tag       *string `json:"tag" validate:"omitempty,min=1,max=255"`
-	Username  *string `json:"username" validate:"omitempty,min=1,max=255"`
-	Password  *string `json:"password" validate:"omitempty,min=1,max=255"`
-	SecretKey *string `json:"secret_key" validate:"omitempty,min=1,max=255"`
-	Params    *string `json:"params" validate:"omitempty,min=1,max=10000"`
+	Username  *string `json:"username" validate:"omitempty,min=0,max=255"`
+	Password  *string `json:"password" validate:"omitempty,min=0,max=255"`
+	SecretKey *string `json:"secret_key" validate:"omitempty,min=0,max=255"`
+	Params    *string `json:"params" validate:"omitempty,min=0,max=10000"`
 }
 
 func (dto *UpdateContainerDto) GetUpdates() (map[string]interface{}, []string) {
