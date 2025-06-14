2e93bd2a165f25d332067d0b7beb6a28dc4536c3
SerheyJ <s.typilow@gmail.com>
2025-06-14 20:36:51 +0300

    Update validation rules in CreateContainerDto to allow nullable fields
    - Modified validation rules for Username, Password, SecretKey, and Params fields to permit empty values by changing the minimum length from 1 to 0.
    - This change enhances flexibility in the CreateContainerDto, allowing for optional input for these fields.

--- modules/containers/dto/create-container.dto.go
+++ modules/containers/dto/create-container.dto.go
@@ -7,10 +7,10 @@ type CreateContainerDto struct {
 	Registry  string `json:"registry" validate:"omitempty,min=1,max=255"`
 	Image     string `json:"image" validate:"required,min=1,max=255"`
 	Tag       string `json:"tag" validate:"omitempty,min=1,max=255"`
-	Username  string `json:"username" validate:"omitempty,min=1,max=255"`
-	Password  string `json:"password" validate:"omitempty,min=1,max=255"`
-	SecretKey string `json:"secret_key" validate:"omitempty,min=1,max=255"`
-	Params    string `json:"params" validate:"omitempty,min=1,max=10000"`
+	Username  string `json:"username" validate:"omitempty,min=0,max=255"`
+	Password  string `json:"password" validate:"omitempty,min=0,max=255"`
+	SecretKey string `json:"secret_key" validate:"omitempty,min=0,max=255"`
+	Params    string `json:"params" validate:"omitempty,min=0,max=10000"`
 }
 
 func ValidateCreateContainerDto(dto CreateContainerDto) error {
