1f3c2efce4ed997be6bcc3e2f92e663b4b9fb8c8
SerheyJ <s.typilow@gmail.com>
2025-06-27 13:37:35 +0300

    Update RunScriptDto to make EnvID optional
    - Changed the validation for EnvID in RunScriptDto from "required" to "omitempty" to allow for optional environment ID in script execution requests.

--- modules/execute/dto/run-script.dto.go
+++ modules/execute/dto/run-script.dto.go
@@ -3,6 +3,6 @@ package dto
 type RunScriptDto struct {
 	ScriptID uint `json:"script_id" validate:"required"`
 	ServerID uint `json:"server_id" validate:"required"`
-	EnvID    uint `json:"env_id" validate:"required"`
+	EnvID    uint `json:"env_id" validate:"omitempty"`
 	LoadEnv  bool `json:"load_env" validate:"required"`
 }
